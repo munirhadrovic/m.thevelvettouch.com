fdee78a8f86645a47e5939b433d4288fa2635a66
novicaa <novicamiketic@gmail.com>
2015-03-04 14:39:25 +0100

    First commit

---
+++ first/settings.php
@@ -0,0 +1,8 @@
+<?php
+$adresa="110 Chestnut Street Hinsdale, Illinois 60521"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+$telephone=6303234534;// Broj koji se poziva na dugme CALL US
+$theme="b";//Thema koja se aplicira na sve
+$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
